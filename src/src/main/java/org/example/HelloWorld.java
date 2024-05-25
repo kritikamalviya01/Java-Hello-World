b035f1677bf4edf4197640df60ef13a8dd156267
kritikamalviya01 <kritikamalviya123@gmail.com>
2024-05-25 14:14:29 +0530

    Java Series Program 1 Hello World

--- src/src/main/java/org/example/HelloWorld.java
+++ src/src/main/java/org/example/HelloWorld.java
@@ -1,7 +1,6 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello World");
